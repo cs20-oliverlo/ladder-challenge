3ce03830bbfe84ce59b10b50e2879f3b3f7c2c07
oliverlo1 <113052541+oliverlo1@users.noreply.github.com>
2022-11-08 11:03:24 -0700

    awd

--- main.js
+++ main.js
@@ -3,43 +3,46 @@ let ctx = cnv.getContext("2d");
 cnv.width = 800;
 cnv.height = 600;
 
-requestAnimationFrame(draw);
-function draw() {
-    ctx.fillStyle = "black";
-    ctx.fillRect(0, 0, cnv.width, cnv.height);
-
+requestAnimationFrame(animate);
+function animate() {
+    // Inputs
     let x = +document.getElementById("x-value").value;
     let y = +document.getElementById("y-value").value;
     let w = +document.getElementById("w-value").value;
     let h = +document.getElementById("h-value").value;
+    let numRungs = +document.getElementById("number-rungs").value;
 
+    // Number Processing
     let xW = w + x;
     let yH = h + y;
 
-    let numRungs = +document.getElementById("number-rungs").value;
+    // Rung Processing
     let hRung = h / (numRungs + 1);
     let spacer = y + hRung;
-    
+
+    // Draw Background
+    ctx.fillStyle = "black";
+    ctx.fillRect(0, 0, cnv.width, cnv.height);
+
+    // Side Lines
     ctx.lineWidth = 5;
     ctx.strokeStyle = "rgb(255, 0, 0)";
+    ctx.beginPath();
     ctx.moveTo(x, y); // Start at (x1, y1)
     ctx.lineTo(x, yH); // Go to (x2, y2)
     ctx.stroke(); // Draw line
+    ctx.moveTo(xW, y);
+    ctx.lineTo(xW, yH);
+    ctx.stroke();
 
-    ctx.lineWidth = 5;
-    ctx.strokeStyle = "rgb(255, 0, 0)";
-    ctx.moveTo(xW, y); // Start at (x1, y1)
-    ctx.lineTo(xW, yH); // Go to (x2, y2)
-    ctx.stroke(); // Draw line
-
+    // Draw Rungs
     for (i = 0; i < numRungs; i++) {
         ctx.lineWidth = 5;
         ctx.strokeStyle = "rgb(255, 0, 0)";
-        ctx.moveTo(x, spacer); // Start at (x1, y1)
-        ctx.lineTo(xW, spacer); // Go to (x2, y2)
-        ctx.stroke(); // Draw line
+        ctx.moveTo(x, spacer);
+        ctx.lineTo(xW, spacer);
+        ctx.stroke();
         spacer += hRung;
     }
-
-    requestAnimationFrame(draw);
+    requestAnimationFrame(animate);
 }
